dfc859c875cc9c17128cc4b9b839022511e991fc
Felix <felix@scale.at>
2019-12-21 11:13:58 +0100

    Initial Commit

---
+++ yesssman/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  yesssman
+//
+//  Created by Felix on 21.12.19.
+//  Copyright © 2019 scale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
